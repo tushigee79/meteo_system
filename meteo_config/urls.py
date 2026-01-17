5335cf4dbed0585d63ee7656cbf4411a331563b8
tushigee79 <meteor97@gmail.com>
2026-01-17 12:33:50 +0800

    Fix duplicate admin namespace

--- meteo_config/urls.py
+++ meteo_config/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static # Энэ мөр дутуу байсан тул нэмэв
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #
-    path('', admin.site.urls),
+    # Админ зөвхөн нэг удаа
+    path('admin/', admin.site.urls),
+
+    # smart_selects (хэрэв ашиглаж байгаа бол)
+    path("chaining/", include("smart_selects.urls")),
 ]
 
-# Хөгжүүлэлтийн үед статик болон медиа файлуудыг харуулах
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
