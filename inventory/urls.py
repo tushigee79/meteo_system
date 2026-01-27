351645b3a7c0fbb5697081632e4f34a32dc7a28d
tushigee79 <meteor97@gmail.com>
2026-01-27 11:40:04 +0800

    БҮРТГЭЛ: admin dashboard, location map, workflow, migrations шинэчлэлт

--- inventory/urls.py
+++ inventory/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path("map/", views.location_map, name="station_map"),
     path("map/one/", views.station_map_view, name="station_map_one"),
 
+    path("map/location/<int:location_id>/", views.location_map_one, name="location_map_one_by_id"),
+
     # CSV импорт
     path("import-csv/", views.device_import_csv, name="device_import_csv"),
 ]
